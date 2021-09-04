81b3a24d83f16bfcd0be343535521d454d5f24f4
gelfik <bulatgilfanov@gmail.com>
2021-09-04 05:05:58 +0300

    add contacts block for purchase page

--- src/pages/components/educationBlock.js
+++ src/pages/components/educationBlock.js
@@ -91,7 +91,7 @@ const EducationBlock = inject(
                     <Carousel
                         responsive={responsive}
                         ref={carouselRef}
-                        arrows={true}
+                        arrows={false}
                         infinite={true}
                         autoPlay={true}
                         autoPlaySpeed={10000}
