6ae51fa3bd84fc29940cb49131e15ce00fce5fef
Bulat Gilfanov <bulatgilfanov@gmail.com>
2021-11-17 00:18:49 +0300

    Revert "Revert "- fix errors""

--- src/pages/Apanel/Users/components/UsersBlock.js
+++ src/pages/Apanel/Users/components/UsersBlock.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {inject, observer} from "mobx-react";
 import {Link} from "react-router-dom";
 import Spinner from "../../../../components/Spinner";
+import {Row, Col} from "react-bootstrap";
 
 const UsersBlock = inject('userStore', 'ausersListStore')(observer((store) => {
     const {ausersListStore} = store
@@ -25,46 +26,52 @@ const UsersBlock = inject('userStore', 'ausersListStore')(observer((store) => {
 
     const getItemCourses = () => {
         return ausersListStore?.usersData?.results?.map((item, i) =>
-            <div className="UsersList__Item" key={i}>
-                <div className="UsersList__Item__Content">
-                    <div className="UsersList__Item__Wrapper">
-                        <Link className="UsersList__Item__Data" to={`/apanel/users/user${item?.id}`}>
-                            <div className="UsersList__Item__Avatar">
-                                <img src={item?.avatar?.file?.small} alt={`${item?.lastName} ${item?.firstName}`}/>
-                            </div>
-                            <div className="UsersList__Item__Title">
-                                <p>{item?.lastName} {item?.firstName}</p>
-                                <div className="Chips">
-                                    {item?.isTeacher && <div className="Chips__Item">
-                                        Преподаватель
-                                        <span/>
-                                    </div>}
-                                    {item?.isMentor && <div className="Chips__Item">
-                                        Наставник
-                                        <span/>
-                                    </div>}
-                                    {item?.username && <div className="Chips__Item">
-                                        {item?.username}
-                                    </div>}
-                                    {item?.email && <div className="Chips__Item">
-                                        {item?.email}
-                                    </div>}
-                                    {item?.phone && <div className="Chips__Item">
-                                        {item?.phone}
-                                    </div>}
+            <Col md={4}>
+                <div className="UsersList__Item" key={i}>
+                    <div className="UsersList__Item__Content">
+                        <div className="UsersList__Item__Wrapper">
+                            <Link className="UsersList__Item__Data" to={`/apanel/users/user${item?.id}`}>
+                                <div className="UsersList__Item__Avatar">
+                                    <img src={item?.avatar?.file?.small} alt={`${item?.lastName} ${item?.firstName}`}/>
                                 </div>
-                            </div>
-                        </Link>
+                                <div className="UsersList__Item__Title">
+                                    <div
+                                        className="UsersList__Item__Title__Name">{item?.lastName} {item?.firstName}</div>
+                                    <div className="Chips">
+                                        {item?.isTeacher && <div className="Chips__Item">
+                                            Преподаватель
+                                            <span/>
+                                        </div>}
+                                        {item?.isMentor && <div className="Chips__Item">
+                                            Наставник
+                                            <span/>
+                                        </div>}
+                                    </div>
+                                    <div className="UsersList__Item__Title__Links">
+                                        <div
+                                           className={"UsersList__Item__Title__Links__Button"}
+                                           onClick={(e)=>{e.preventDefault();window.open(`${item?.vkLink}`, "_blank")}}>
+                                            <svg aria-hidden="true" height="20" width="20">
+                                                <use xlinkHref={'#icon-vk-2'}/>
+                                            </svg>
+                                            <p> перейти</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Col>
         )
     }
     // {coursesPageStore.spinner.spinnerStatus ? <Spinner type={'local'}/> : <CourseBlock/>}
 
     return (
         <div className="UsersList">
-            {ausersListStore.spinner.spinnerStatus ? <Spinner type={'local'}/> : getUsersList()}
+            <Row>
+                {ausersListStore.spinner.spinnerStatus ? <Spinner type={'local'}/> : getUsersList()}
+            </Row>
         </div>
     )
 }))
